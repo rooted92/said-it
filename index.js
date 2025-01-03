d1a1f1360bea3657e4a42b17b1e40c0723953b9a
Pedro Castaneda <petecast76@gmail.com>
2025-01-03 12:29:38 -0800

    feat: implemented test routes

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    console.log('Hello World');
-    console.dir(req);
+// app.use((req, res) => {
+//     console.log('Hello World');
+//     console.dir(req);
+//     res.send('<h1>Hello World</h1>');
+// });
+
+app.get('/', (req, res) => {
+    console.log('GET request to /');
     res.send('<h1>Hello World</h1>');
-})
+});
+
+app.get('/cats', (req, res) => {
+    console.log('GET request to /cats');
+    res.send('Meow');
+});
+
+app.get('/dogs', (req, res) => {
+    console.log('GET request to /dogs');
+    res.send('Woof');
+});
 
 app.listen(3000, () => {    
     console.log('Server is running on port 3000');
