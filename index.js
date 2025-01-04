d2644c0dea7808fa61b6e293c86744e450eac7a9
Pedro Castaneda <petecast76@gmail.com>
2025-01-04 06:30:44 -0800

    feat: add get request for search

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express');
 
 const app = express();
 
-// app.use((req, res) => {
-//     console.log('Hello World');
-//     console.dir(req);
-//     res.send('<h1>Hello World</h1>');
-// });
-
 app.get('/', (req, res) => {
     console.log('GET request to /');
     res.send('<h1>Hello World</h1>');
@@ -22,6 +16,20 @@ app.get('/dogs', (req, res) => {
     console.log('GET request to /dogs');
     res.send('Woof');
 });
+
+app.get('/search', (req, res) => {
+    let {q} = req.query;
+    if(!q) {
+        res.send('Nothing found if nothing searched.')
+    }
+    res.send(`<h1>Search results for ${q}</h1>`)
+})
+
+// app.use((req, res) => {
+//     console.log('Hello World');
+//     console.dir(req);
+//     res.send('<h1>Hello World</h1>');
+// });
 
 app.listen(3000, () => {    
     console.log('Server is running on port 3000');
